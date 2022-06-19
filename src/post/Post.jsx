8282624e54f1023cbb20ef4f5df327cf6f2e32b1
Brian Sitati <briansit93@gmail.com>
2022-06-19 21:39:58 +0300

    styled the post compenents

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import "./post.css"
 
 export default function Post() {
   return (
-    <div>
-        
+    <div className="post">
+    <img 
+    className="postImg"
+    src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?cs=srgb&dl=pexels-syed-qaarif-andrabi-6685428.jpg&fm=jpg" alt=""/>
+
+    <div className="postInfo">
+        <div className="postCats">
+         <span className="postCat">Technology</span>
+         <span className="postCat">Sports</span>
+        </div>
+        <span className="postTitle">Lorem ipsum dolor sit
+        </span>
+        <hr/>
+        <span className="postDate">1 hour ago</span>
+    </div>
+    <p className="postDesc">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, laborum. 
+        Accusantium quae accusamus ipsa cupiditate. Quia, dignissimos. 
+        Maiores minima quo magni tenetur. Accusamus corporis cum enim error at? Rerum, amet?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, laborum. 
+        Accusantium quae accusamus ipsa cupiditate. Quia, dignissimos. 
+        Maiores minima quo magni tenetur. Accusamus corporis cum enim error at? Rerum, amet?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, laborum. 
+        Accusantium quae accusamus ipsa cupiditate. Quia, dignissimos. 
+        Maiores minima quo magni tenetur. Accusamus corporis cum enim error at? Rerum, amet?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, laborum. 
+        Accusantium quae accusamus ipsa cupiditate. Quia, dignissimos. 
+        Maiores minima quo magni tenetur. Accusamus corporis cum enim error at? Rerum, amet?
+    </p>
     </div>
   )
 }
