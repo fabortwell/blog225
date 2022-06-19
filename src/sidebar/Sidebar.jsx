9460c2d35d20f0429777477c3466f24c4a08c0c7
Brian Sitati <briansit93@gmail.com>
2022-06-19 18:51:56 +0300

    added about me

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+ import "./sidebar.css"
+ 
+ export default function Sidebar() {
+   return (
+     <div className="sidebar">
+     <div className="sidebarItem">
+       <span className="sidebarTitle">ABOUT ME</span>
+       <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt=""/>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero similique cupiditate soluta, deserunt cumque aliquam animi at? Voluptatibus quasi quisquam neque ex cum eos perferendis quos repellat velit, sint atque!</p>
+     </div>
+     </div>
+   )
+ }
+ 
