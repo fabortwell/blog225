16b6d80c659babbc8be76d33825be5d58231359a
Brian Sitati <briansit93@gmail.com>
2022-06-19 19:00:25 +0300

    added categories section of the blogs

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -8,6 +8,18 @@
        <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt=""/>
        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero similique cupiditate soluta, deserunt cumque aliquam animi at? Voluptatibus quasi quisquam neque ex cum eos perferendis quos repellat velit, sint atque!</p>
      </div>
+     <div className="sidebarItem">
+     <span className="sidebarTitle">CATEGORIES</span>
+     <ul className="sidebarList">
+       <li className="sidebarListItem">Technology</li>
+       <li className="sidebarListItem">Sports</li>
+       <li className="sidebarListItem">Music</li>
+       <li className="sidebarListItem">Travel</li>
+       <li className="sidebarListItem">DIY</li>
+       <li className="sidebarListItem">Fashion</li>
+     </ul>
+     </div>
+     
      </div>
    )
  }
