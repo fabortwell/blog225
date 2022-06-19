8990af3a36730742a1d42af7d0885f0f4e09bfae
Brian Sitati <briansit93@gmail.com>
2022-06-19 22:41:02 +0300

    fixed a bug in the header extion adding z index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import TopBar from "./components/topbar/TopBar";
-import Home from "./components/pages/home/Home";
+import Single from "./components/pages/single/Single";
 
 
 function App() {
   return (
      <>
      <TopBar/>
-     <Home />
+     <Single />
      </>
   );
 }
---
+++ src/components/pages/single/Single.jsx
@@ -0,0 +1,17 @@
+
+import Sidebar from "../../sidebar/Sidebar"
+import "./single.css"
+
+const Single = () => {
+  return (
+    <div className="single">
+          {/*post*/}
+          <Sidebar />
+    </div>
+  )
+}
+
+export default Single
+  
+
+
