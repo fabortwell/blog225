9b3cf446889ffce6e8fe7f27d0a2573706405950
Brian Sitati <briansit93@gmail.com>
2022-06-19 18:08:57 +0300

    created a sidebar component

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
     <div className='header'>
       <div className="headerTitles">
        <span className="headerTitlesSm">Node & React </span>
-       <span className="headerTitlesLg">Redux </span>
+       <span className="headerTitlesLg">Blog </span>
       </div>
-        <img className="headerImg" src=" https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="" />
+        <img className="headerImg" src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
     </div>
   )
 }
