6bacbd06873fb51ea6c56ff3018d75ad2edccfe1
Brian Sitati <briansit93@gmail.com>
2022-06-19 17:14:08 +0300

    added an image for a blog

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,7 +4,11 @@ import "./header.css"
 export default function Header() {
   return (
     <div className='header'>
-      header
+      <div className="headerTitles">
+       <span className="headerTitlesSm">Node & React </span>
+       <span className="headerTitlesLg">Redux </span>
+      </div>
+        <img className="headerImg" src=" https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" alt="" />
     </div>
   )
 }
