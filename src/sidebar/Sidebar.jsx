7807fd01516c4e142b0526059e8e2baf54d8086e
Brian Sitati <briansit93@gmail.com>
2022-06-19 19:14:27 +0300

    added some padding and styling to the aboiut us section

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -22,10 +22,10 @@
      <div className="sidebarItem">
      <span className="sidebarTitle">FOLLOW US</span>
      <div className="sidebarSocial">
-     <i className="topIcon fa-brands fa-facebook-square"></i>
-    <i className="topIcon fa-brands fa-twitter-square"></i>
-    <i className="topIcon fa-brands fa-pinterest-square"></i>
-    <i className="topIcon fa-brands fa-instagram-square"></i>
+      <i className="sidebarIcon fa-brands fa-facebook-square"></i>
+      <i className="sidebarIcon fa-brands fa-twitter-square"></i>
+      <i className="sidebarIcon fa-brands fa-pinterest-square"></i>
+      <i className="sidebarIcon fa-brands fa-instagram-square"></i>
      </div>
      </div>
      </div>
