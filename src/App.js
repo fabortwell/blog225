5fe5ef7981f2c9a82e2642906e88e142a6ca1662
Brian Sitati <briansit93@gmail.com>
2022-06-20 10:29:44 +0300

    styled the register and signin section

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import TopBar from "./components/topbar/TopBar";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import Settings from "./components/pages/settings/Settings";
+import Login from "./components/pages/login/Login";
+
+
+
 function App() {
   return (
      <>
      <TopBar/>
-     <Settings />
+     <Login />
+     {/* <Settings /> */}
      {/* <Write /> 
       <Home />
      <Single /> */}
