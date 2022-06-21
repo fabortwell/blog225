abf274259bbf49258c2c1e0e5f16d637cb5d528e
Brian Sitati <briansit93@gmail.com>
2022-06-21 22:23:33 +0300

    added an error page

--- src/App.js
+++ src/App.js
@@ -6,22 +6,50 @@ import Write from "./components/pages/write/Write";
 import Settings from "./components/pages/settings/Settings";
 import Login from "./components/pages/login/Login";
 import Register from "./components/pages/register/Register";
+import ErrorPage from "./components/pages/ErrorPage";
 
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-     <>
-     <TopBar/>
-     <Register />
-     {/* <Login /> */}
-     {/* <Settings /> */}
-     {/* <Write /> 
-      <Home />
-     <Single /> */}
-     </>
+  
+    <Router>
+        <TopBar/>
+      <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/write" element={<Write />} />
+       <Route path="/login" element={<Login />} />
+       <Route path="/register" element={<Register />} />
+       <Route path="/single" element={<Single />} />
+       <Route path="/settings" element={<Settings />} />
+       <Route path="*" element={<ErrorPage />}/>
+      </Routes>
+    </Router>
+    // <Router>
+    //   <TopBar />
+    //   <Routes>
+    //     <Routes exact path="/">
+    //       <Home />
+    //     </Routes>
+    //     {/* <Route path="/posts">
+    //       <Home />
+    //     </Route>
+    //     <Route path="/register">
+    //       {currentUser ? <Home /> : <Register />}
+    //     </Route>
+    //     <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+    //     <Route path="/post/:id">
+    //       <Single />
+    //     </Route>
+    //     <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+    //     <Route path="/settings">
+    //       {currentUser ? <Settings /> : <Login />}
+    //     </Route>
+    //   </Switch>
+    // </Router> */}
   );
 }
 
 export default App;
-
 
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,30 +1,56 @@
+import {Link} from "react-router-dom";
 import "./topbar.css"
 
 export default function TopBar() {
+  const user = true;
   return (
     <div className="top">
     <div className="topLeft">
-    <i className="topIcon fa-brands fa-facebook-square"></i>
-    <i className="topIcon fa-brands fa-twitter-square"></i>
-    <i className="topIcon fa-brands fa-pinterest-square"></i>
-    <i className="topIcon fa-brands fa-instagram-square"></i>
+      Techy-Convo 
     </div>
     <div className="topCenter">
         <ul className="topList">
-         <li className="topListItem">HOME</li>
-         <li className="topListItem">ABOUT</li>
-         <li className="topListItem">CONTACT</li>
-         <li className="topListItem">WRITE</li>
-         <li className="topListItem">LOGIN</li>
-         <li className="topListItem">REGISTER</li>
-         </ul>      
+         <li className="topListItem"> 
+           <Link className="link" to="/">HOME</Link>
+           </li>
+         <li className="topListItem">
+         <Link className="link" to="/">ABOUT</Link>
+         </li>
+         <li className="topListItem">
+         <Link className="link" to="/">CONTACT</Link>
+         </li>
+         <li className="topListItem">
+         <Link className="link" to="/">WRITE</Link>
+         </li>
+         {user && <li className="topListItem">LOGOUT</li>}
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
     </div>
-    <div className="topRight">
-        <img 
-        className="topImg"
-        src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-    </div>
-  </div>
-  )
+  );
 }
+ 
